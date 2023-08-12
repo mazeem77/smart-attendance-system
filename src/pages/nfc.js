c375f31d617f85084cf3852c65295737e5b38893
Muhammad Azeem <80689390+mazeem77@users.noreply.github.com>
2023-08-12 22:22:20 +0500

    Added verification and Registration Process

--- src/pages/nfc.js
+++ src/pages/nfc.js
@@ -28,7 +28,7 @@ function App() {
       console.log(response)
       if (response.status === 200) {
         setLog("Verified!")
-        setMessage(response.data.username)
+        setMessage("response.data.username")
       } else {
         setLog("Error!")
       }
